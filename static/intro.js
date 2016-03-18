fe62765f51bf788d44930ad65dea138902ad4ebd
svg153 <svg153@gmail.com>
2016-03-18 19:38:12 +0100

    Se identa el codigo. Se añaden algunas clases y se quitan algunos textos para que la app los carge en el intro.js de los json para poder tener varios idiomas.

--- static/intro.js
+++ static/intro.js
@@ -1,22 +1,38 @@
 // -- DOM -- //
-//Info tab
+// Info tab
 var welcomePage                = $('.welcome-page');
 var welcomePageLogin           = $('.welcome-page .ui-btn.big');
 
-var setInitialTexts = function() {
+
+var slideBar                   = $('.wl-sidebar');
+var inbox                      = $('#inbox');
+var windowContent              = $('.wl-window');
+
+
+
+
+var setWelcomeTexts = function() {
 	welcomePage.find('.welcome-first').text(lang.welcomeTitle);
 	welcomePage.find('.welcome-second').text(lang.welcomeSubtitle);
 	welcomePageLogin.find('.ellipsis').text(lang.welcomeLogin);
 }
 
+var setAppText = function() {
+	slideBar.find('.ui-navgroup-title-txt').text(lang.listTitle);
+	inbox.find('.ui-navgroup-element-txt').text(lang.inbox);
+	slideBar.find('.btn-new-list').text(lang.newList);
+	windowContent.find('.wl-newtask-placeholder')[0].placeholder=lang.newTask;
+}
+
 // funcion main
 var init = function(){
-	setInitialTexts();
+	setWelcomeTexts();
 	wql.getTasks(function(error, message) {
 		if (message.length===0) {
 			welcomePage.show();
 		}
 	})
+	setAppText();
 }
 
 
